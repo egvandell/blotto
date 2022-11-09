e90ccbc66a41e198ac852e6c91fb4465ce8b570e
Greg Van Dell <gvandell@engineation.com>
2022-11-09 09:09:07 -0500

    Lottery picking WIP

--- contracts/Blotto.sol
+++ contracts/Blotto.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+
 
 error Lottery_NotEnoughTokensSent();
 error Lottery_NeedMoreTokensInsufficientBalance();
@@ -16,64 +20,92 @@ interface Token {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (uint256);    
 }
 
+//contract Blotto is VRFConsumerBaseV2, Pausable, Ownable, ReentrancyGuard {
 contract Blotto is Pausable, Ownable, ReentrancyGuard {
-    enum LotteryState {
-        Open,
-        Drawing
-    }
+    using SafeMath for uint256;
+    address[] private s_ticketAddresses;
 
     struct TicketInfo {
         uint16 lottery_id;
-        uint256 purchaseTS;
-        uint256 amount;
-        bool winner;
+        uint256 totalTokens;
     }
 
-    Token private blotToken;
-    LotteryState public s_lotteryState;
-    uint16 public storage s_lottery_id;     // needs to be incremented for each lottery    
+    Token public blotToken;
+    bool public s_lotteryStateOpen;
+    uint16 public s_lottery_id;
 
-    event BoughtTicket(address indexed from, uint256 amount);
+    address constant public CHARITY_ADDRESS_HERE = 0x0000000000000000000000000000000000000000;
+    address constant public DAO_ADDRESS_HERE = 0x0000000000000000000000000000000000000000;
 
-    mapping(address => uint256) public addressToken;
+    mapping(uint16 => mapping(address => TicketInfo)) private s_ticketInfos;
+    mapping(uint16 => uint256) private s_ticketsBought;
 
-    mapping(address => TicketInfo) public ticketinfos;  // need to review this data structure
+//    mapping(address => uint256) public addressToken;
 
-/* a lottery is the data structure thats mapped
-n lotteries
+    event BoughtTicket(uint16 indexed s_lottery_id, address indexed from, uint256 amount);
+    event WinnerPicked(uint16 indexed s_lottery_id, address indexed winner);
 
-lottery_id
-lottery_date
-lottery_winner
-
-ticket_id
-ticket_purchase_date
-lottery_id
-total tickets purchased?
-
- need to determine how the winner wins.  is it a 1-1 on a token?  token price can change
-
-
-*/
     constructor(Token _tokenAddress) {
         require(address(_tokenAddress) != address(0),"Token Address cannot be address 0");                
         blotToken = _tokenAddress;
-        s_lotteryState = LotteryState.Open;
+        s_lotteryStateOpen = true;
     }
 
     function buyTicket(uint256 tokenAmount) external payable whenNotPaused {
         if (tokenAmount == 0) { revert Lottery_NotEnoughTokensSent(); }
         if (blotToken.balanceOf(_msgSender()) >= tokenAmount) { revert Lottery_NeedMoreTokensInsufficientBalance(); }
-        if (s_lotteryState != LotteryState.Open) { revert Lottery_LotteryNotOpen(); }
+        if (!s_lotteryStateOpen) { revert Lottery_LotteryNotOpen(); }
 
         blotToken.transferFrom(_msgSender(), address(this), tokenAmount);
-        addressToken[_msgSender()] += tokenAmount;
 
-        emit BoughtTicket(_msgSender(), tokenAmount);
+
+
+
+        for (uint256 i=0; i < tokenAmount; i++) s_ticketAddresses.push(_msgSender());
+
+
+
+
+//        s_ticketInfos[s_lottery_id][_msgSender()].totalTokens += tokenAmount;
+//        s_ticketsBought[s_lottery_id] += tokenAmount;
+
+        emit BoughtTicket(s_lottery_id, _msgSender(), tokenAmount);
     }
 
-//    function drawing() {}
+//    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override {
+/*
 
+// add a require / revert to make sure at least 1 ticket was purchased
+
+//        address[] memory tokenAddressTickets;
+
+//        for (uint i=0; i<s_ticketInfos[s_lottery_id].length; i++) 
+//        {
+//            for (uint j=0; j<s_ticketInfos[s_lottery_id][i].totalTokens; j++)
+//                tokenAddressTickets.push = s_ticketInfos[s_lottery_id][i];
+//        }
+        uint256 totalTickets = s_ticketAddresses.length;
+
+        uint256 indexOfWinner = randomWords[0] % s_ticketAddresses.length;
+        address winner = s_ticketAddresses[indexOfWinner];
+
+        // transfer tokens: 80% to winner, 19% to charity, 1% to DAO
+        uint256 winner_tokens = totalTickets.div(100).mul(80);
+        uint256 charity_tokens = totalTickets.div(100).mul(190);
+        uint256 dao_tokens = totalTickets.sub(winner_tokens).sub(charity_tokens);        
+
+        s_lotteryStateOpen = true;
+
+//        s_ticketAddresses = new address[];
+
+//        s_lastTimeStamp = block.timestamp;
+        blotToken.transferFrom(address(this), winner, winner_tokens);
+        blotToken.transferFrom(address(this), CHARITY_ADDRESS_HERE, charity_tokens);
+        blotToken.transferFrom(address(this), DAO_ADDRESS_HERE, dao_tokens);
+
+        emit WinnerPicked(s_lottery_id, winner);
+    }
+*/
 
     function pause() external onlyOwner {
         _pause();
