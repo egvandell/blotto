f7653e6339c3307948a9663c1764f5071c729588
GREG VVAN DELL <gvandell@engineation.com>
2022-12-13 08:14:38 -0500

    test being called wrong.  needed flag --deploy-fixture

--- test/example.js
+++ test/example.js
@@ -3,8 +3,11 @@ const {deployments} = require('hardhat');
 describe('Blotto', () => {
   it('testing 1 2 3', async function () {
     await deployments.fixture(['Blotto']);
-    const Blotto = await deployments.get('Blotto');
-    console.log(`Blotto.address for example.js found at${Blotto.address}`);
+    const {tokenOwner} = await getNamedAccounts();
+    const BlottoContract = await ethers.getContract('Blotto', tokenOwner);
+
+    //    const Blotto = await deployments.get('Blotto');
+    console.log(`BlottoContract.address for example.js found at${BlottoContract.address}`);
 //    const ERC721BidSale = await deployments.get('ERC721BidSale');
 //    console.log({ERC721BidSale});
   });
