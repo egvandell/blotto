4b35a8c5bef1b10b0da034c53c190b7218eff5ee
Greg Van Dell <gvandell@engineation.com>
2022-12-28 14:48:33 -0500

    VRF Coordinator Mock - WIP

---
+++ deploy/02-deploy-vrfCoordinatorV2Mock.js
@@ -0,0 +1,28 @@
+const { networkConfig } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments}) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    
+    const args = [
+        process.env.TOKEN_ADDRESS, 
+        networkConfig[chainId]["automationUpdateInterval"],
+        networkConfig[chainId]["vrfCoordinatorV2"], 
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["subscriptionId"],
+        networkConfig[chainId]["callbackGasLimit"], 
+    ]
+
+    const blotto = await deploy("Blotto", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmation: 6,
+    })
+
+    console.log(
+        `Deployed Blotto.sol to ${blotto.address} with ${process.env.TOKEN_ADDRESS} as BlottoToken address`
+      );
+}
