b80820b83297b264d00cae86d9f887b0cd14c661
Greg Van Dell <gvandell@engineation.com>
2023-01-24 16:24:09 -0500

    interval change

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -11,7 +11,7 @@ const networkConfig = {
         subscriptionId: "6275",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        automationUpdateInterval: "600",
+        automationUpdateInterval: "60",
         callbackGasLimit: "10000000", // 10,000,000 gas  --> raised to 10mm from .5mm bc low gas caused timeout
         BASE_FEE: "25000000000000000",
         GAS_PRICE_LINK: "1000000000",
